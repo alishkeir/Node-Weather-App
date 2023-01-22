abe3a997e6e5c93252cbe1885d3ef017cf9f28f4
alishkeir <alishkeir_@hotmail.com>
2023-01-22 17:29:06 +0200

    Refactoring code with callbacks

--- app.js
+++ app.js
@@ -1,66 +1,72 @@
 const chalk = require('chalk');
 const request = require('request');
+const geocode = require('./utils/geocode');
 
 require('dotenv').config();
 
 const URL = `http://api.weatherstack.com/current?access_key=${process.env.WS_API_KEY}&query=Beirut`;
 const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Beirut.json?limit=1&access_token=${process.env.MB_ACCESS_TOKEN}`;
 
-// Weather Forecast
-request({ uri: URL, json: true }, (error, response, body) => {
-    if (error?.errno === -3008) {
-        console.log(chalk.red('Please check your Internet Connection!'));
-        return false;
-    }
-    if (body?.error) {
-        switch (body.error.type) {
-            case 'invalid_access_key':
-                console.log(chalk.red('Invalid API Key'));
-                break;
-            case 'missing_query':
-                console.log(chalk.red('The given location is not valid'));
-                break;
+// // Weather Forecast
+// request({ uri: URL, json: true }, (error, response, body) => {
+//     if (error?.errno === -3008) {
+//         console.log(chalk.red('Please check your Internet Connection!'));
+//         return false;
+//     }
+//     if (body?.error) {
+//         switch (body.error.type) {
+//             case 'invalid_access_key':
+//                 console.log(chalk.red('Invalid API Key'));
+//                 break;
+//             case 'missing_query':
+//                 console.log(chalk.red('The given location is not valid'));
+//                 break;
 
-            default:
-                console.log(chalk.red('Error:\n'), body);
-                break;
-        }
+//             default:
+//                 console.log(chalk.red('Error:\n'), body);
+//                 break;
+//         }
 
-        return false;
-    }
+//         return false;
+//     }
 
-    console.log(body.current);
-});
+//     console.log(body.current);
+// });
 
-// Geocoding;
-request({ uri: geocodeURL, json: true }, (error, response, body) => {
-    let lat = '';
-    let long = '';
+// // Geocoding;
+// request({ uri: geocodeURL, json: true }, (error, response, body) => {
+//     let lat = '';
+//     let long = '';
 
-    if (error?.errno === -3008) {
-        console.log(chalk.red('Please check your Internet Connection!'));
-        return false;
-    }
+//     if (error?.errno === -3008) {
+//         console.log(chalk.red('Please check your Internet Connection!'));
+//         return false;
+//     }
 
-    if (body?.error) {
-        console.log(chalk.red('Error:\n'), error);
-        return false;
-    }
+//     if (body?.error) {
+//         console.log(chalk.red('Error:\n'), error);
+//         return false;
+//     }
 
-    if (!body.query) {
-        console.log(chalk.red(body.message));
-        return false;
-    }
+//     if (!body.query) {
+//         console.log(chalk.red(body.message));
+//         return false;
+//     }
 
-    if (!body.query?.length) {
-        console.log(chalk.red('The given location is not valid'));
-        return false;
-    }
+//     if (!body.query?.length) {
+//         console.log(chalk.red('The given location is not valid'));
+//         return false;
+//     }
 
-    if (body?.features?.length) {
-        if (body?.features[0]?.center) {
-            lat = body?.features[0]?.center[1];
-            long = body?.features[0]?.center[0];
-        }
-    }
+//     if (body?.features?.length) {
+//         if (body?.features[0]?.center) {
+//             lat = body?.features[0]?.center[1];
+//             long = body?.features[0]?.center[0];
+//         }
+//     }
+// });
+
+geocode('Beirut', (error, data) => {
+    console.log(error);
+    console.log(data);
 });
---
+++ utils/geocode.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+const geocode = (address, callback) => {
+    const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+        address
+    )}.json?limit=1&access_token=${process.env.MB_ACCESS_TOKEN}`;
+
+    request({ uri: geocodeURL, json: true }, (error, response, body) => {
+        if (error) {
+            callback('Unable to connect to location services!');
+        } else if (body?.features?.length === 0) {
+            callback('The given location is not valid, try another location');
+        } else {
+            callback(null, {
+                lat: body.features[0].center[1],
+                long: body.features[0].center[0],
+                location: body.features[0].matching_place_name,
+            });
+        }
+    });
+};
+
+module.exports = geocode;
