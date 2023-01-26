80c22bbb6a8c7710ad25990f816457b88ee639cd
alishkeir <alishkeir_@hotmail.com>
2023-01-26 14:33:05 +0200

    Adde HTTP Endpoint using express

--- app.js
+++ app.js
@@ -1,34 +1,69 @@
 const chalk = require('chalk');
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
 
 const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
 
 require('dotenv').config();
 
-const address = process.argv[2];
+const app = express();
 
-if (address) {
-    geocode(address, (error, { lat, long, location } = {}) => {
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '/public');
+const viewsPath = path.join(__dirname, '/templates/views');
+const partialsPath = path.join(__dirname, '/templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Weather App',
+    });
+});
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!',
+        });
+    }
+
+    geocode(req.query.address, (error, { lat, long, location } = {}) => {
         if (error) {
-            console.log(chalk.red(`Error: ${error}`));
-            return;
+            return res.send({ error });
         }
-
-        console.log('\n', chalk.cyan(location));
 
         forecast(lat, long, (error, forecastData) => {
             if (error) {
-                console.log(chalk.red(`Error: ${error}`));
-                return;
+                return res.send({ error });
             }
 
-            console.log(forecastData);
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address,
+            });
         });
     });
-} else {
-    console.log(
-        '\n',
-        chalk.red('Please provide an address in the arguments, ex:'),
-        chalk.magenta('"node app.js London"')
-    );
-}
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Not Found',
+        errorMessage: 'Page not found.',
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.');
+});
